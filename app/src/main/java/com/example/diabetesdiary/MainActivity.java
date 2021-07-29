610d9adbcaee0f10614db513aa19f74985e3d8e2
ksv97 <kosov.r.m@gmail.com>
2021-07-29 16:39:15 +0300

    finished creating view for add diary item activity

--- app/src/main/java/com/example/diabetesdiary/MainActivity.java
+++ app/src/main/java/com/example/diabetesdiary/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.diabetesdiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
