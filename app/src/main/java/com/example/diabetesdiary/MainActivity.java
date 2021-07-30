0cf0fc76de5674d38f699a4868d028d56baab228
ksv97 <kosov.r.m@gmail.com>
2021-07-31 00:29:00 +0300

    add bottom navigation

--- app/src/main/java/com/example/diabetesdiary/MainActivity.java
+++ app/src/main/java/com/example/diabetesdiary/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diabetesdiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -7,13 +8,22 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.Locale;
 import java.util.TimeZone;
 
 
 /* TODO
-
+    !! Дописать метод setupValues в классе addDiaryItemFragment
+    1. Переместить кнопку вниз
+    2. Сделать так, чтобы новый элемент не добавлялся без заполнения хотя бы какого-либо значения
+    3. Сделать отображение значений сверху на SeekBar-ах
+    4. Пофиксить баг со значением 0.9 на сикбаре (там почему-то 0.9000004)
  */
 public class MainActivity extends FragmentActivity {
 
@@ -26,9 +36,41 @@ public class MainActivity extends FragmentActivity {
         Locale.setDefault(new Locale("ru"));
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+3"));
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        DiaryListFragment listFragment = new DiaryListFragment();
-        transaction.add(R.id.list_container,listFragment);
-        transaction.commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        NavigationBarView.OnItemSelectedListener listener = new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.diary:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.list_container, new DiaryListFragment())
+                                .commit();
+                        break;
+                    case R.id.statistics:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.list_container, new StatisticsFragment())
+                                .commit();
+                        break;
+                    case R.id.preferences:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.list_container, new PreferencesFragment())
+                                .commit();
+                        break;
+                }
+
+                return true;
+            }
+        };
+
+        bottomNavigationView.setOnItemSelectedListener(listener);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.list_container, new DiaryListFragment())
+                .commit();
     }
+
 }
