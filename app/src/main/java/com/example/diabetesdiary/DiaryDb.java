28f6e49c9a618267c95dfa62e0a511e2ebff9504
ksv97 <kosov.r.m@gmail.com>
2021-07-30 09:32:21 +0300

    add inserting and selecting all methods

--- app/src/main/java/com/example/diabetesdiary/DiaryDb.java
+++ app/src/main/java/com/example/diabetesdiary/DiaryDb.java
@@ -1,10 +1,14 @@
 package com.example.diabetesdiary;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class DiaryDb {
 
@@ -16,14 +20,14 @@ public class DiaryDb {
     public static final String COLUMN_DATE = "Date";
     public static final String COLUMN_SUGAR_LEVEL = "SugarLevel";
     public static final String COLUMN_CARB_UNITS = "CarbUnits";
-    public static final String COLUMN_INSULIN = "Insulin";
+    public static final String COLUMN_INSULIN_RAPID = "InsulinRapid";
     public static final String COLUMN_NOTE = "Note";
 
     public static final int NUM_COLUMN_ID = 0;
     public static final int NUM_COLUMN_DATE = 1;
     public static final int NUM_COLUMN_SUGAR_LEVEL = 2;
     public static final int NUM_COLUMN_CARB_UNITS = 3;
-    public static final int NUM_COLUMN_INSULIN = 4;
+    public static final int NUM_COLUMN_INSULIN_RAPID = 4;
     public static final int NUM_COLUMN_NOTE = 5;
 
     private SQLiteDatabase database;
@@ -31,8 +35,6 @@ public class DiaryDb {
     public DiaryDb(Context ctx) {
         database = new DbOpenHelper(ctx).getWritableDatabase();
     }
-
-
 
     private class DbOpenHelper extends SQLiteOpenHelper {
 
@@ -44,10 +46,10 @@ public class DiaryDb {
         public void onCreate(SQLiteDatabase db) {
             String query = "CREATE TABLE " + DIARY_ITEMS_TABLE_NAME + " ("
                     + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + COLUMN_DATE + " TEXT NOT NULL,"
+                    + COLUMN_DATE + " INTEGER NOT NULL,"
                     + COLUMN_SUGAR_LEVEL + " REAL,"
                     + COLUMN_CARB_UNITS + " REAL,"
-                    + COLUMN_INSULIN + " REAL,"
+                    + COLUMN_INSULIN_RAPID + " REAL,"
                     + COLUMN_NOTE + " TEXT)";
             database.execSQL(query);
         }
@@ -58,6 +60,36 @@ public class DiaryDb {
             onCreate(db);
         }
 
+        public long insert(DiaryItem item) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_DATE, item.getDate().getTimeInMillis());
+            values.put(COLUMN_SUGAR_LEVEL, item.getSugarLevel());
+            values.put(COLUMN_CARB_UNITS, item.getCarbUnits());
+            values.put(COLUMN_INSULIN_RAPID, item.getInsulinRapidCount());
+            values.put(COLUMN_NOTE, item.getNote());
+            return database.insert(DIARY_ITEMS_TABLE_NAME,null,values);
+        }
+
+
+        public ArrayList<DiaryItem> selectAll() {
+            Cursor cursor = database.query(DIARY_ITEMS_TABLE_NAME,null,null,null,null,null,null);
+            ArrayList<DiaryItem> items = new ArrayList<>();
+
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                int id = cursor.getInt(NUM_COLUMN_ID);
+                long dateInMillis = cursor.getLong(NUM_COLUMN_DATE);
+                float sugarLevel = cursor.getFloat(NUM_COLUMN_SUGAR_LEVEL);
+                float carbUnits = cursor.getFloat(NUM_COLUMN_CARB_UNITS);
+                float insulinRapid = cursor.getFloat(NUM_COLUMN_INSULIN_RAPID);
+                String note = cursor.getString(NUM_COLUMN_NOTE);
+                DiaryItem item = new DiaryItem(id,insulinRapid, carbUnits, sugarLevel,note,dateInMillis);
+                items.add(item);
+            }
+            cursor.close();
+            return items;
+
+        }
 
     }
 
